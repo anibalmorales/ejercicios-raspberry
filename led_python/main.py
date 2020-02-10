8b621c8b82cc86a391e7a020919a54584efbadae
Luis Cabrera Benito <parzibyte@gmail.com>
2020-02-09 18:12:16 -0600

    Create main.py

---
+++ led_python/main.py
@@ -0,0 +1,28 @@
+"""
+    Programado por Luis Cabrera Benito 
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+    
+    Blog:       https://parzibyte.me/blog
+    Ayuda:      https://parzibyte.me/blog/contrataciones-ayuda/
+    Contacto:   https://parzibyte.me/blog/contacto/
+"""
+import RPi.GPIO as GPIO
+import time
+ESPERA = 0.5
+PIN = 10
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN, GPIO.OUT)
+while True:
+    GPIO.output(PIN, GPIO.HIGH)
+    time.sleep(ESPERA)
+    GPIO.output(PIN, GPIO.LOW)
+    time.sleep(ESPERA)
+GPIO.cleanup()
